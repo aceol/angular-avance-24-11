3064cc46cc4b681013600af3f23d12765e20fca1
Antoine Ceol <antoine.ceol@gmail.com>
2024-11-14 14:41:29 +0100

    Lab 5 standalone step 2 (bootstraping)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
+import { AlertComponent } from './alert/alert.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        MenuComponent,
+        AlertComponent,
+        RouterOutlet,
+        FooterComponent,
+    ],
 })
 export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+import { provideHttpClient } from '@angular/common/http';
+import { WELCOME_MSG } from './app/app.token';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(),
+        {
+            provide: WELCOME_MSG,
+            useValue: 'Bienvenue sur Zenika Ecommerce',
+        },
+    ]
+})
   .catch((err) => console.error(err));
