a5f20aebbacddd3e2e6372882c572c76fec49a39
Antoine Ceol <antoine.ceol@gmail.com>
2024-11-15 09:57:31 +0100

    Lab 7 - forms

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,27 +1,20 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Customer } from '../customer/customer.types';
-import { BasketItem } from './basket.types';
-import { BasketService } from './basket.service';
-import { Observable } from 'rxjs';
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketFormComponent } from './basket-form.component';
+import { BasketService } from './basket.service';
+import { BasketItem } from './basket.types';
 
 @Component({
-    selector: 'app-basket',
-    templateUrl: './basket.component.html',
-    standalone: true,
-    imports: [
-    AsyncPipe,
-    CurrencyPipe
-],
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  standalone: true,
+  imports: [AsyncPipe, CurrencyPipe, BasketFormComponent],
 })
-export class BasketComponent implements OnInit{
-
+export class BasketComponent implements OnInit {
   ngOnInit(): void {
     this.basketService.fetch().subscribe();
   }
-
-  protected customer: Customer = { name: '', address: '', creditCard: '' };
 
   protected get basketTotal$(): Observable<number> {
     return this.basketService.total;
@@ -31,20 +24,9 @@ export class BasketComponent implements OnInit{
     return this.basketService.items$;
   }
 
-
   protected get length$(): Observable<number> {
     return this.basketService.numberOfItems$;
   }
 
   private basketService = inject(BasketService);
-  private router = inject(Router);
-
-  protected checkout(event: Event): void {
-    event.stopPropagation();
-    event.preventDefault();
-
-    this.basketService
-    .checkout(this.customer)
-    .subscribe(() => this.router.navigate(['']))
-  }
 }
